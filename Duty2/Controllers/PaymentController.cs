54e96176a01bc2eb007489789268b1750270cd4e
frisha0088 <grisha0088@gmail.com>
2017-01-20 16:04:03 +0700

    Final enhances

--- Duty2/Controllers/PaymentController.cs
+++ Duty2/Controllers/PaymentController.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web.DynamicData;
 using System.Web.Http;
-using System.Web.Http.Cors;
 using Duty2.DTOs;
 using Duty2.Helpers;
 using Duty2.Models;
